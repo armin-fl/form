778d464e66b5844cd466684d2adfaca5a1cb5886
armin-fl <f.armin8090@gmail.com>
2024-09-04 11:05:53 +0330

    custom mdb classes added to the form

--- myform/forms.py
+++ myform/forms.py
@@ -6,5 +6,10 @@ class PersonForm(forms.ModelForm):
         model = Person
         fields = ['name', 'family_name', 'age', 'selection', 'nationality', 'date_of_birth']
         widgets = {
-            'date_of_birth': forms.HiddenInput()  # We'll handle this with JavaScript
+            'name': forms.TextInput(attrs={'id': 'name', 'class': 'form-control border ' }),
+            'family_name': forms.TextInput(attrs={'id': 'family_name', 'class': 'form-control border'}),
+            'age': forms.NumberInput(attrs={'id': 'age', 'class': 'form-control border'}),
+            'selection': forms.Select(attrs={'id': 'selection', 'class': 'form-control border'}),
+            'nationality': forms.CheckboxInput(attrs={'id': 'nationality', 'class': 'form-check-input ' }),
+            'date_of_birth': forms.HiddenInput()  # This will remain hidden and handled by JavaScript
         }
